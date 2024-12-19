dfbd9aa3da0a0bc8b9fa5ff98b8c76926a5fc157
Sachin Gupta <nicrooctal16@gmail.com>
2024-12-19 13:29:31 +0530

    web optimised

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,9 @@
 module.exports = {
     assets: ['./assets/fonts'], // Path to your fonts folder
+    expo: {
+        name: 'The-Catalyst-Tree',
+        web: {
+          bundler: 'webpack'
+        }
+      }
 };
