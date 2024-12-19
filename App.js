952f5d67a425bb303392521c12f1de72431d94eb
Sachin Gupta <nicrooctal16@gmail.com>
2024-12-19 13:17:46 +0530

    web optimised

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-
 import React from "react";
-import {SafeAreaView, ScrollView } from "react-native";
+import { SafeAreaView, ScrollView, Platform } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import Home from "./screens/homePage";
 import AboutUs from "./screens/aboutUs";
 import Debt from "./screens/debt";
@@ -12,30 +12,50 @@ import Investor from "./screens/investor";
 import MergersAcquisitions from "./screens/ma";
 import Acceleration from "./screens/acceleration";
 
-const Stack = createNativeStackNavigator();
+// Use different navigator based on platform
+const Stack = Platform.select({
+  web: createStackNavigator(),
+  default: createNativeStackNavigator(),
+});
 
 export default function App() {
   return (
-    <SafeAreaView style={{flex: 1}}>
-        {/* <Home></Home> */}
-        {/* <AboutUs></AboutUs> */}
-        {/* <Debt></Debt> */}
-        {/* <Equity></Equity> */}
-        {/* <Startup></Startup> */}
-        {/* <Investor></Investor> */}
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="AboutUs" component={AboutUs} />
-                <Stack.Screen name="Debt" component={Debt} />
-                <Stack.Screen name="Equity" component={Equity} />
-                <Stack.Screen name="Startup" component={Startup} />
-                <Stack.Screen name="Investor" component={Investor} />
-                <Stack.Screen name="MergersAcquisitions" component={MergersAcquisitions} />
-                <Stack.Screen name="Acceleration" component={Acceleration} />
-            </Stack.Navigator>
-        </NavigationContainer>
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
+      <NavigationContainer
+        linking={{
+          config: {
+            screens: {
+              Home: '',
+              AboutUs: 'about-us',
+              Debt: 'debt',
+              Equity: 'equity',
+              Startup: 'startup',
+              Investor: 'investor',
+              MergersAcquisitions: 'mergers-acquisitions',
+              Acceleration: 'acceleration',
+            },
+          },
+        }}
+      >
+        <Stack.Navigator 
+          initialRouteName="Home" 
+          screenOptions={{ 
+            headerShown: false,
+            ...(Platform.OS === 'web' && {
+              animationEnabled: true // Disable animations on web
+            })
+          }}
+        >
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="AboutUs" component={AboutUs} />
+          <Stack.Screen name="Debt" component={Debt} />
+          <Stack.Screen name="Equity" component={Equity} />
+          <Stack.Screen name="Startup" component={Startup} />
+          <Stack.Screen name="Investor" component={Investor} />
+          <Stack.Screen name="MergersAcquisitions" component={MergersAcquisitions} />
+          <Stack.Screen name="Acceleration" component={Acceleration} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
-};
-
+}
--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import { registerRootComponent } from 'expo';
-
+import { AppRegistry, Platform } from 'react-native';
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+const appName = "The-Catalyst-Tree";
+
+// Register the app for both web and native platforms
+if (Platform.OS === 'web') {
+  if (window.document) {
+    AppRegistry.registerComponent(appName, () => App);
+    AppRegistry.runApplication(appName, {
+      initialProps: {},
+      rootTag: document.getElementById('root')
+    });
+  }
+} else {
+  // For iOS and Android
+  registerRootComponent(App);
+}
+
+export default App;
