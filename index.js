62cb0bac6afbba6365011f7aebfa82cf52d5a6d7
Sachin Gupta <nicrooctal16@gmail.com>
2024-12-19 13:33:54 +0530

    web optimised

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
+import { Platform } from 'react-native';
 import { registerRootComponent } from 'expo';
-import { AppRegistry, Platform } from 'react-native';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const appName = "The-Catalyst-Tree";
-
-// Register the app for both web and native platforms
 if (Platform.OS === 'web') {
-  if (window.document) {
-    AppRegistry.registerComponent(appName, () => App);
-    AppRegistry.runApplication(appName, {
-      initialProps: {},
-      rootTag: document.getElementById('root')
-    });
-  }
+  const root = createRoot(document.getElementById('root'));
+  root.render(<App />);
 } else {
-  // For iOS and Android
   registerRootComponent(App);
 }
-
-export default App;
