67cfc4165ea0f3ecbd3924406712dc4339ee26e3
Akankshi-gupta <akankshigp12345@gmail.com>
2025-01-06 12:57:30 +0530

    changed footer

--- components/footer.js
+++ components/footer.js
@@ -15,21 +15,26 @@ export default function Footer({navigation}){
             }
     });
     return(
-        <View style={{backgroundColor: '#0E0E0E', paddingHorizontal: '8%', paddingTop: '7%'}}>
-            <Image source={require('../assets/images/footer-logo.png')} style={{marginBottom: '4%', height: 120, width: 120}} resizeMode="contain"></Image>
-            <View style={{flexDirection: 'row', justifyContent: 'space-between', marginBottom: '4%'}}>
-                <TouchableOpacity style={{borderRadius: 8, backgroundColor: '#FFFFFF40', borderColor: '#D6D6D6', borderWidth: 1, width:minWidth?"auto" :'20%', paddingVertical: '1%',paddingHorizontal:minWidth?"1%":""}}>
-                    <Text style={footerStyles.smallText}>Instagram</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={{borderRadius: 8, backgroundColor: '#FFFFFF40', borderColor: '#D6D6D6', borderWidth: 1, width: '20%', paddingVertical: '1%'}}>
-                    <Text style={footerStyles.smallText}>Behance</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={{borderRadius: 8, backgroundColor: '#FFFFFF40', borderColor: '#D6D6D6', borderWidth: 1, width: '20%', paddingVertical: '1%'}}>
-                    <Text style={footerStyles.smallText}>Twitter</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={{borderRadius: 8, backgroundColor: '#FFFFFF40', borderColor: '#D6D6D6', borderWidth: 1, width: '20%', paddingVertical: '1%'}}>
-                    <Text style={footerStyles.smallText}>Dribble</Text>
-                </TouchableOpacity>
+        <View style={{backgroundColor: '#0E0E0E', paddingHorizontal: '8%', paddingTop: '6%'}}>
+            <View style={{flexDirection: 'row', marginBottom: '4%', alignItems: 'center', justifyContent: 'space-between'}}>
+                <View style={{flexDirection: 'row', alignItems: 'center', width: '50%'}}>
+                    <Image source={require('../assets/images/footer-logo.png')} style={{height: 120, width: 120, marginRight: '2%'}} resizeMode="contain"></Image>
+                    <Text style={{fontSize: 19, fontWeight: 600, color: 'rgba(106, 204, 52, 1)', }}>The Catalyst Tree</Text>
+                </View>
+                <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '20%', alignItems: 'center',}}>
+                    <TouchableOpacity>
+                        <Image source={require('../assets/images/instagram-vector.png')} style={{height: 24, width: 24}} resizeMode="contain"></Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={require('../assets/images/twitter-vector.png')} style={{height: 24, width: 24}} resizeMode="contain"></Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={require('../assets/images/facebook-vector.png')} style={{height: 24, width: 24}} resizeMode="contain"></Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={require('../assets/images/whatsapp-vector.png')} style={{height: 24, width: 24}} resizeMode="contain"></Image>
+                    </TouchableOpacity>
+                </View>
             </View>
             <View style={{flexDirection: 'row', justifyContent: 'space-between', marginBottom: '4%'}}>
                 <View style={{marginTop: '4%'}}>
@@ -44,6 +49,9 @@ export default function Footer({navigation}){
                     <TouchableOpacity style={{marginBottom: '8%'}} onPress={() => navigation.navigate("AboutUs")}>
                         <Text style={[footerStyles.smallText, {color: '#808080', textAlign: 'start',}]}>About Us</Text>
                     </TouchableOpacity>
+                    <TouchableOpacity style={{marginBottom: '8%'}}>
+                        <Text style={[footerStyles.smallText, {color: '#808080', textAlign: 'start',}]}>Pricing</Text>
+                    </TouchableOpacity>
                     <TouchableOpacity style={{marginBottom: '8%'}} onPress={() => navigation.navigate("FAQ")}>
                         <Text style={[footerStyles.smallText, {color: '#808080', textAlign: 'start',}]}>FAQ's</Text>
                     </TouchableOpacity>
@@ -57,7 +65,7 @@ export default function Footer({navigation}){
                 <View style={{width: '45%'}}>
                     <Text style={[styles.headingText, {fontSize: minWidth?14:20, marginBottom: '4%'}]}>Subscribe Our Newsletter.</Text>
                     <View style={{flexDirection:minWidth?"column" :'row', justifyContent: 'space-between'}}>
-                        <TextInput  style={{backgroundColor: '#FFFFFF', width: '62%', paddingVertical: '2%', borderRadius:minWidth?5: 8,paddingHorizontal:"2%",marginBottom:minWidth?5:"auto",fontSize:minWidth?10:15}}></TextInput>
+                        <TextInput placeholder="Email address" placeholderTextColor={'rgba(214, 214, 214, 1)'} style={{backgroundColor: '#FFFFFF', width: '62%', paddingVertical: '2%', borderRadius:minWidth?5: 8,paddingHorizontal:"2%",marginBottom:minWidth?5:"auto",fontSize:minWidth?10:15}}></TextInput>
                         <TouchableOpacity style={{backgroundColor: '#FFFFFF', borderRadius: minWidth?5:8,  width: minWidth?'50%':"30%", alignItems: 'center', justifyContent: 'center'}} >
                             <Text style={[footerStyles.smallText, {color: '#0E0E0E' , paddingVertical: '2%'}]}>Book a Demo</Text>
                         </TouchableOpacity>
